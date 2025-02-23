76810fd1e736e4b5274a930c93cac9cf63bb5c5f
tnxl <tosinoyinboblessed@gmail.com>
2025-02-23 15:28:00 +0100

    Just found out I implemented inverse of GF256 wrong, reimplemented with exponentiation-based approach instead of the Euclidean approach I used before

--- polynomial-encoding/src/main.rs
+++ polynomial-encoding/src/main.rs
@@ -43,23 +43,19 @@ impl GF256 {
     }
 
     fn inverse(&self) -> GF256 {
-        let mut a: u16 = self.0 as u16;
-        let mut b: u16 = 0x11B; // Modulus rrstriction
-        let mut x0: u16 = 1;
-        let mut x1: u16 = 0;
+        let mut result = GF256(1);
+        let mut base = *self;
+        let mut exp = 254; // a^(2^8 - 2) is the inverse in this case
 
-        while a > 1 {
-            let q = a / b; // Quotient
-            let (new_a, new_b) = (b, a%b);
-            let (new_x0, new_x1) = (x1, x0 ^ Self::gf_mul(q as u8, x1 as u8) as u16);
-
-            a = new_a;
-            b = new_b;
-            x0 = new_x0;
-            x1 = new_x1;
+        while exp > 0 {
+            if exp & 1 != 0 {
+                result = result.mul(base);
+            }
+            base = base.mul(base);
+            exp >>= 1;
         }
 
-        GF256(x0 as u8)
+        result
     }
 
     fn gf_mul(a: u8, b: u8) -> u8 {
@@ -147,7 +143,7 @@ impl fmt::Display for Polynomial {
 }
 
 fn main() {
-    /*let a = GF256(7);
+    let a = GF256(7);
     let b = GF256(3);
 
     println!("a = {}", a);
@@ -155,7 +151,7 @@ fn main() {
     println!("a + b = {}", a.add(b));
     println!("a * b = {}", a.mul(b));
     println!("a⁻¹ = {}", a.inverse());
-    println!("b⁻¹ = {}", b.inverse()); */
+    println!("b⁻¹ = {}", b.inverse());
 
     let p1 = Polynomial::new(vec![GF256(3), GF256(7), GF256(7)]); // should return 3 + 5x + 7x²
     let p2 = Polynomial::new(vec![GF256(1), GF256(2)]); // should return 1 + 2x
